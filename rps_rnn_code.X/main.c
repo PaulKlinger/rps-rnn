ff94920d96b7a8d9aefe059a96b06d214b904bc3
Paul Klinger <klinger.paul@gmail.com>
2019-07-06 14:30:34 +0200

    added 2nd mode (use rps-rnn to play against another person)

--- rps_rnn_code.X/main.c
+++ rps_rnn_code.X/main.c
@@ -5,6 +5,7 @@
  * Created on June 23, 2019, 8:41 PM
  */
 
+#include <avr/io.h>
 #include "mcc_generated_files/config/clock_config.h"
 #include "mcc_generated_files/mcc.h"
 #include "mcc_generated_files/include/pin_manager.h"
@@ -21,7 +22,7 @@
 //E:\Program Files (x86)\Microchip\MPLABX\v5.10\packs\Atmel\ATtiny_DFP\1.3.238\gcc\dev\attiny1614\device-specs\specs-attiny1614
 // from "-Tdata 0x803800" to "	-Tdata 0x802000"
 // this prevents a linker error for static or global variables.
-// But I'm very uncertain if this is actually correct.
+// But I'm somewhat uncertain if this is actually correct.
 
 
 //LED pins:
@@ -75,10 +76,22 @@ static void display_move(rps move) {
     }
 }
 
-int main(void)
-{
-    /* Initializes MCU, drivers and middleware */
-    SYSTEM_Initialize();
+rps get_selected_move() {
+    if (rock_pressed()) {
+        return ROCK;
+    } else if (paper_pressed()) {
+        return PAPER;
+    } else if (scissors_pressed()) {
+        return SCISSORS;
+    } else {
+        return START;
+    }
+}
+
+void play_against_rps_rnn() {
+    // human player plays against device
+    // pressing start starts a countdown until device move is shown
+    // then player move needs to be entered before start can be pressed again
     
     blink_all(2, 100);
     
@@ -98,16 +111,57 @@ int main(void)
             blink_all(3, 200);
             display_move(our_move);
             opponent_move_entered = false;
-        } else if (rock_pressed()) {
-            opponent_move = ROCK;
-            opponent_move_entered = true;
-        } else if (paper_pressed()) {
-            opponent_move = PAPER;
-            opponent_move_entered = true;
-        } else if (scissors_pressed()) {
-            opponent_move = SCISSORS;
-            opponent_move_entered = true;
+        } else {
+            rps selected_move = get_selected_move();
+            if (selected_move != START) {
+                opponent_move = selected_move;
+                opponent_move_entered = true;
+            }
         }
         _delay_ms(10);
+    }
+}
+
+void play_with_rps_rnn() {
+    // use device to suggest moves against human opponent
+    // entering opponents move immediately suggests next move
+    
+    // Wait until start button is released
+    // This gives some randomness to seed the RNG
+    while (start_pressed()){};
+    // seed the RNG with the current time (from RTC counter)
+    srandom(RTC.CNT);
+    
+    display_move(simple_model_predict(START, 0.1));
+    rps selected_move;
+    while (1) {
+        // need to save selected move, otherwise it could change between
+        // condition evaluation and prediction
+        selected_move = get_selected_move();
+        if (selected_move != START) {
+            // blink the LED so it's obviously a new move even if the
+            // new suggestion is the same as the old one
+            set_all_leds(0);
+            _delay_ms(100);
+            display_move(simple_model_predict(selected_move, 0.1));
+            // wait until button is released + a bit for bouncing
+            while (get_selected_move() != START){}
+            _delay_ms(100);
+        }
+        _delay_ms(10);
+    }
+}
+
+int main(void)
+{
+    /* Initializes MCU, drivers and middleware */
+    SYSTEM_Initialize();
+    // wait a bit to avoid spurious input on startup
+    _delay_ms(100);
+    // choose device mode depending on whether start is pressed
+    if (start_pressed()) {
+        play_with_rps_rnn();
+    } else {
+        play_against_rps_rnn();
     }
 }
